269c0a502d72d3e0d1e1f4550f0b9cc2a2a54097
Alfonso Torrez <alftaurus@gmail.com>
2022-12-26 14:52:14 -0800

    Day 03 Complete
    Turning in work for day 03.

---
+++ python03/main.py
@@ -0,0 +1,17 @@
+"""
+The Ultimate Wacky Recipe Maker
+
+We have learned enough skills for a simple, but cool, project!
+
+Remember when you were a kid and thought the ideal dinner 
+would just be all your favorite things mixed in a bowl? How did that Nutella Mac & Cheese taste? Well - let's come up with a recipe generator to build us an amazing dish for today's evening meal!
+"""
+foodType = "chinese food"
+plantType = "daisy"
+cookingMethod = "steamed"
+desc = "denied"
+item = "fork"
+
+print("Menu \n Today we have some", cookingMethod, "\n", 
+     foodType, " that is served \n with a", plantType, " and", 
+     item, ".\n Our food is never", desc, "!")
--- python03/summary.md
+++ python03/summary.md
@@ -1,7 +1,11 @@
 #Day 03 - Concat...what?
 
 ##Objective        
-- 
+- Learn how to concatenate string variables
+- Combining print statements with variables
+- Common bugs
+- The Ultimate Wacky Recipe Maker Challenge 
 
 ##Summary 
-- 
+- Explored different ways to concatenate strings and 
+variables.  
