a6abb6fbfbfa9555d29d9c2f672b14842654e4af
Alfonso Torrez <alftaurus@gmail.com>
2023-01-06 18:50:42 -0800

    Learning elifs
    Learning how to use elif statements.

---
+++ python06/sign_in.py
@@ -0,0 +1,11 @@
+username = input("username >  ")
+password = input("password > ")
+
+if username == "ben" and password == "gg":
+  print("Hello World!")
+elif username == "john" and password == "yikes123":
+  print("Hello, Bruh")
+elif username == "jose" and password == "11":
+  print("Go home!")
+else: 
+  print("Leave NOW!")
--- python06/summary.md
+++ python06/summary.md
@@ -1,7 +1,8 @@
-#Day 06 - Python Learning
+#Day 06 - What the elif is this?
 
 ##Objective        
-- 
+- Learning ```elif``` statements
 
 ##Summary 
-- 
+- Created several examples using ```elif``` statements. Reviewed 
+common errors. 
