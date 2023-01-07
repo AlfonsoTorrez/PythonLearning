d3ff62b142d528a10cd2b018f6fe7b85bef71504
Alfonso Torrez <alftaurus@gmail.com>
2023-01-06 18:22:49 -0800

    Guessing Game
    Guessing game made with if and else statements.

---
+++ python05/main.py
@@ -0,0 +1,13 @@
+#guessing game using if statements
+
+answer = input("Do you like 'hanging around'?: ")
+if answer == "yes":
+  print("You are Spiderman!")
+else:
+  print("Then you are not Spiderman")
+
+answer = input("Do you like 'fly high'?: ")
+if answer == "yes":
+  print("You are Superman!")
+else:
+  print("Then you are not Superman")
